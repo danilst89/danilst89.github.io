a9c8126f1fe599853bfcead86a0769bdd2fc4807
Danil <danilst1990@gmail.com>
2021-09-26 18:37:31 +0200

    added dino

--- SUSHI/js/script.js
+++ SUSHI/js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 window.addEventListener('DOMContentLoaded', () => {
     let hamburger = document.querySelector('.header__hamburger');
     let headerUl = document.querySelector('#headerUl');
@@ -8,15 +7,4 @@ window.addEventListener('DOMContentLoaded', () => {
         headerUl.classList.toggle('header__list_active');
         logo.classList.toggle('none');
     });
-=======
-window.addEventListener('DOMContentLoaded', () => {
-    let hamburger = document.querySelector('.header__hamburger');
-    let headerUl = document.querySelector('#headerUl');
-    let logo = document.querySelector('.header__logo');
-
-    hamburger.addEventListener('click', () => {
-        headerUl.classList.toggle('header__list_active');
-        logo.classList.toggle('none');
-    });
->>>>>>> 0832ea9b5529a42b2045a6278e9c214a76d7cccf
 });
